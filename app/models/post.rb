9ca57cb6d0164020b77d32547c053a783c200356
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-17 04:28:39 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,19 +1,4 @@
 
 class Post < ActiveRecord::Base
-<<<<<<< HEAD
 
 end
-=======
-  attr_accessor :name, :content 
-  POSTS = []
-  
-  def initialize
-      
-  end
-  
-  def create(name:, content:)
-    
-  end 
-  
-end
->>>>>>> 137661a321d8f0a087646468dd51408cdd4b7d8d
